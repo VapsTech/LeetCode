9a4a0f474e8a26c50d21a749eeaf8e49a55a70cc
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-06 15:43:43 +0100

    Time: 21 ms (34.15%), Space: 24.1 MB (6.23%) - LeetHub

---
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cpp
@@ -0,0 +1,57 @@
+//VapsTech | 08/06/2024
+//Complexity: O(n)
+//Runtime: 
+#include <cmath>
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> pos;
+        vector<int> neg;
+        int closest;
+ 
+        //Create the list with positive nums and list with neg nums
+        for (int i = 0; i < n; ++i){
+            if (nums[i] == 0){
+                return 0;
+            }
+            if (nums[i] > 0){
+                pos.push_back(nums[i]);
+
+            } else {
+                neg.push_back(nums[i]);
+            }
+        }
+
+        //Find smallest in positive list
+        int closest_pos = pow(10,5);
+        for (int i = 0; i < pos.size(); ++i){
+            if (pos[i] < closest_pos){
+                closest_pos = pos[i];
+            }
+        }
+
+        //Find greatest (closest to zero) in negative list 
+        int closest_neg = pow(-10,5);
+        for (int i = 0; i < neg.size(); ++i){
+            if (neg[i] > closest_neg){
+                closest_neg = neg[i];
+            }
+        }
+
+        if (pos.empty()){
+            return closest_neg;
+        }
+        if (neg.empty()){
+            return closest_pos;
+        }
+
+        //Check smallest between smallest positive list and smallest negative list
+        if (closest_pos <= abs(closest_neg)){
+            return closest_pos;
+        } else {
+            return closest_neg;
+        }
+    }
+};
