4740ce7ad640aecd48d451fe84a1a4d738d43f23
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-06 11:52:32 +0100

    Time: 4 ms (22.36%), Space: 7.9 MB (42.32%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,26 @@
+//VapsTech | 08/06/2024
+//Complexity: O(n)
+//Runtime: 
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        int size = max(n1, n2); //Get max size
+
+        string ans_arr;
+
+        for (int i = 0; i < size; ++i){
+            if (i < n1) {
+                ans_arr.push_back(word1[i]); //Append first if it's in range
+            }
+            if (i < n2) { 
+                ans_arr.push_back(word2[i]); //Append second if it's in range
+            }
+        }
+
+        return ans_arr;
+        
+    }
+};
