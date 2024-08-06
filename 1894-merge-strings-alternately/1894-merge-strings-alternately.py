afab4974025b5e29e3fe1f9e5a8db31d002e7aa8
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-06 12:01:40 +0100

    Time: 39 ms (28.72%), Space: 16.5 MB (28.2%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        length = 0
+        mergeWord = []
+
+        length = max(len(word1), len(word2)) #find longest string
+
+        for i in range(length):
+            if (i < len(word1)): #check if index is not out of range to append the char
+                mergeWord.append(word1[i])
+            if (i < len(word2)): #check if index is not out of range to append the char
+                mergeWord.append(word2[i])
+
+        return ''.join(mergeWord) #turn list into a string
+
+#Complexity: O(n)
+        
+
+
+                
