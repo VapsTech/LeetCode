7f8890d3550c2fb3dd74a2c9b7faf84466d8febd
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-07 18:52:57 +0100

    Time: 277 ms (33.96%), Space: 27.1 MB (5.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,33 +1,29 @@
 #VapsTech && Alef-Keuffer | 08/07/2024
-#Complexity: 
+#Complexity: O(n)
 #Runtime: 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
 
         pre = [1] * n
-
         suf = [1] * n
-
         ans = [1] * n
 
-        #Prefix Array: OK
+        #Prefix Array
         mult = 1
         for i in range(1,n):
-            mult *= nums[i-1]
+            mult *= nums[i-1] #multiplying from first index to second last index
             pre[i] = mult
         pre[0] = 1 #First index equal to 1
 
 
-        #Sufix Array: OK
+        #Sufix Array
         mult = 1
         for i in range(n-1):
-            mult *= nums[n-1-i] #FIXME
-            suf[i+1] = mult
-        suf[0] = 1 #Last index equal to 1
+            mult *= nums[n-1-i] #Multiplying from last index to first
+            suf[i+1] = mult #Start from second index
+        suf[0] = 1 #First index equal to be last index equal 1 when reversed
         suf = list(reversed(suf))
-
-
 
         #Multiply prefix with sufix:   
         for i in range(n):
