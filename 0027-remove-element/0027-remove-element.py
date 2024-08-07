dcd65617b34925ba36e3995b5cedab08c9106cf6
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-07 19:41:28 +0100

    Time: 33 ms (81.91%), Space: 16.4 MB (94.49%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -6,9 +6,9 @@ class Solution:
         n = len(nums) #Get total length 
 
         count = 0
-        for item in nums:
-            if item != val: #If it is not the val 
-                nums[count] = item #Change nums without val
+        for i in range(n):
+            if nums[i] != val: #If it is not the val 
+                nums[count] = nums[i] #Change nums without val
                 count += 1 #Increment index
 
         return count #Return length of new arr without val
