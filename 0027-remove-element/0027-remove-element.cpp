b3dbeddd3509fde8585bd85f2aca58fa0c5a21e9
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-07 20:14:14 +0100

    Time: 0 ms (100%), Space: 10.7 MB (34.55%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,19 @@
+//VapsTech | 08/07/2024
+//Complexity: O(n)
+//Runtime: 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int count{0};
+
+        for (int i = 0; i < n; i++){
+            if (nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
