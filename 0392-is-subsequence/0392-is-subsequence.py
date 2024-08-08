b8be7b4e2ca9f8eb1b2f7f1c247cc6310e7bf0ba
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-08-08 09:34:13 +0100

    Time: 34 ms (70.78%), Space: 16.7 MB (5.15%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,17 @@
+#VapsTech | 08/08/2024
+#Complexity: O(n)
+#Runtime: 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_size = len(s)
+        t_size = len(t)
+        s_index = t_index = 0
+
+        while t_index < t_size and s_index < s_size: #run until index is out of range for any of the strs
+            if (s[s_index] == t[t_index]): #check if s char is in some t index
+                s_index += 1
+            t_index += 1
+        
+        return s_index == s_size
+            
+
