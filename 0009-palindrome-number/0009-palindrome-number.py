e532fbb5269a4c3eb0b742035741f372b2fbf225
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-09-25 01:00:33 +0100

    Time: 59 ms (34.19%), Space: 16.6 MB (6.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+#VapsTech | 09/24/2024
+#Runtime:
+#Complexity: 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x) #Turn into a str object
+        x_reversed = ''.join(reversed(x)) #Join from right to left 
+
+        return x == x_reversed
+        
