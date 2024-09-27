530fe9abd607966c8cf569a8b1ff331d9f1a7697
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-09-27 23:39:34 +0100

    Time: 33 ms (72.55%), Space: 16.5 MB (54.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,14 +3,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        opening = ("(", "[", "{")
-        closing = {"(":")" , "[":"]" , "{":"}" }
+        values = {"(":")" , "[":"]" , "{":"}" } 
 
         valid_closing = []
 
         for char in s:
-            if char in closing: #Check if it is a opening
-                valid_closing.append(closing[char]) #Append to the Stack
+            if char in values: #Check if it is a opening
+                valid_closing.append(values[char]) #Append to the Stack
                 continue 
             
             if char not in valid_closing or char != valid_closing.pop(): #Pop the Stack 
