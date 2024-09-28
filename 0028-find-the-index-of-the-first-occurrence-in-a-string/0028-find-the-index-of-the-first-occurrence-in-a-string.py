cb08113d97247d4a7d34575236a240402815fa84
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-09-29 00:34:47 +0100

    Time: 26 ms (95.85%), Space: 16.5 MB (26.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,3 +1,5 @@
+#VapsTech
+#Complexity: O(m*n)
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
 
@@ -7,18 +9,19 @@ class Solution:
 
         while idx < len(haystack):
             
-            if (haystack[idx] == needle[count]):
+            if (haystack[idx] == needle[count]): #Check if char is same
                 compare.append(haystack[idx])
-                count += 1
+                count += 1 #Increment needle's index
             else:
-                if count > 0:
-                    idx = idx - count
+                if count > 0: 
+                    idx = idx - count #Reset idx to one position after the attempted needle
+                #Reset compare list & needle's index
                 count = 0
                 compare = []
 
 
-            if (len(compare) == len(needle)):
-                return idx + 1 - len(needle)
+            if (len(compare) == len(needle)): #Check if we found all chars from needle
+                return idx + 1 - len(needle) #Return the first idx position the needle
         
             idx += 1
 
