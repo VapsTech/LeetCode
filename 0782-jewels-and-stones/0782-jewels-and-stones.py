73afe7a40931e71ef9aa26515dd62c3f3d290d5a
Victor Soares <134007281+VapsTech@users.noreply.github.com>
2024-11-29 19:57:12 -0300

    Time: 0 ms (100%), Space: 17.3 MB (9%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,3 +1,14 @@
+#VapsTech | 11/28/2024
+#Complexity: O(n+m) 
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+
+        jewels_set = set(jewels)
+
+        for stone_index in range(len(stones)):
+            if stones[stone_index] in jewels_set: #Access jewels in O(1)
+                count += 1            
+        
+        return count
         
